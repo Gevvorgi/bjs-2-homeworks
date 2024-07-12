430cc12f843804a25446701532705054d4a0a1a0
Gevvorgi <sanya.asayev@mail.ru>
2024-07-12 03:14:56 +0300

    Init Homeworks

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
   
+let dis = (b**2-4*a*c);
+if(dis < 0) {
+  arr = [];
+} else if(dis === 0) {
+  let disD = -b/(2*a)
+  arr = [disD];
+} else if(dis > 0) {
+  let dis1 = (-b + Math.sqrt(dis) )/(2*a);
+  let dis2 = (-b - Math.sqrt(dis) )/(2*a);
+  arr = [dis1, dis2]
+}
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+  let p = (percent / 100) / 12
+  let s = amount - contribution;
+  let paymentMonth = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+  let payoutTotal = paymentMonth * countMonths;
+
+  return +payoutTotal.toFixed(2)
 }
